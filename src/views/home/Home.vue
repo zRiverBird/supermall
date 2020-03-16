be73adfc9c481344ed10afb6a370aefb03990fc8
zRiverBird <774986925@qq.com>
2020-03-16 15:17:59 +0800

    初步是实现轮播图

---
+++ src/views/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="home" class="home-nav">
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar';
+import {getHomeMultidata} from 'network/home';
+export default {
+    name:"Home",
+    components:{
+      NavBar
+    },
+    data(){
+      return {
+        banners : [],
+        recommends :[]
+      }
+    },
+    created(){
+      getHomeMultidata().then(res=>{
+        console.log(res);
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommends.list;
+      })
+    }
+}
+</script>
+
+<style scoped>
+.home-nav{
+  background-color: var(--color-tint);
+  color :#fff;
+}
+</style>
